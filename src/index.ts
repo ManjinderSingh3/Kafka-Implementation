9913715bfbc7c8285aea20031c07ad6082f9e3e9
Manjinder <msinghw3@gmail.com>
2025-02-19 22:02:48 -0600

    Raw code of Kafka producer and consumer in Nodejs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { offset } from "highcharts";
 import { Kafka } from "kafkajs";
 const kafka = new Kafka({
   clientId: "my-first-kafka-app",
@@ -6,3 +7,29 @@ const kafka = new Kafka({
 
 const producer = kafka.producer();
 const consumer = kafka.consumer({ groupId: "consumer-group-1" });
+
+const produce = async () => {
+  await producer.connect();
+  await producer.send({
+    topic: "topic-1",
+    messages: [{ value: "Hello from Nodejs" }],
+  });
+
+  await consumer.connect();
+  await consumer.subscribe({ topic: "topic-1", fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      if (!message.value) {
+        return;
+      }
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value.toString(),
+      });
+    },
+  });
+};
+
+produce().catch(console.error);
